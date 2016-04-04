07b307840d54c01823e1a34ad20fb1c9c177e817
Jordan Thomas <jthom143@jhu.edu>
2016-04-04 16:18:01 -0400

    Begun flux analysis. Sucessfully calculated fluxes/closure for one grid point

---
+++ data/make_datafiles.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+files=`ls /datascope/pradal-esm/${1}/history/*ocean_month.nc`
+
+var="temp_xflux_adv temp_yflux_adv temp_zflux_adv temp temp_tendency neutral_temp temp_submeso temp_nonlocal_KPP sw_heat temp_vdiffuse_impl"
+
+for v in $var
+  do
+  echo $v
+  i=0
+  for f in $files
+    do
+    num=`printf %03d $i`
+    ncra -v $v $f ~/control_aredi800/data/var${num}_tmp.nc
+    let i=i+1
+  done
+  files_ann=`ls ~/control_aredi800/data/var*_tmp.nc`
+  ncrcat $files_ann ~/control_aredi800/data/$v.nc
+  rm var*_tmp.nc
+done
+
