f46bbd55ee860bfd4f0745ca3a500f04584ef810
Jordan Thomas <jthom143@jhu.edu>
2016-04-18 12:26:59 -0400

    detrended SH ohc in timeseries

--- python/plot_aredi_global_ohc.py
+++ python/plot_aredi_global_ohc.py
@@ -53,7 +53,7 @@ global_heat_anomaly = {}
 global_heat_detrend = {}
 heat_SH = {}
 heat_SH_anomaly = {}
-
+heat_SH_detrend = {}
 for name in names:
     heat[name], heat_sumz[name], heat_global[name] = calc_ohc(temp[name], rhodzt[name], area[name])
     global_heat_mean[name] = heat_global[name].collapsed('time', iris.analysis.MEAN)
@@ -68,6 +68,7 @@ for name in names:
     heat_SH[name] = b.collapsed(['longitude', 'latitude'], iris.analysis.SUM)
     mean_SH = heat_SH[name].collapsed('time', iris.analysis.MEAN)
     heat_SH_anomaly[name] = heat_SH[name] - mean_SH
+    heat_SH_detrend[name] = signal.detrend(heat_SH_anomaly[name].data)
 
 fig1 = plt.figure(figsize = (6, 8))
 ax1 = fig1.add_axes([0.1, 0.75, 0.8, 0.15])
@@ -87,18 +88,18 @@ ax3.set_xlim([0,500])
 fig1 = plt.figure(figsize = (6, 8))
 ax1 = fig1.add_axes([0.15, 0.75, 0.75, 0.15])
 ax1.plot((global_heat_detrend['aredi_400'])/1e22, color = 'k', label = 'aredi = 400 m$^2$s$^{-1}$')
-ax1.plot(heat_SH_anomaly['aredi_400'].data/1e22, color = 'k', ls = '--')
+ax1.plot(heat_SH_detrend['aredi_400']/1e22, color = 'k', ls = '--')
 plt.title('(a) A$_{redi}$ = 400 m$^2$s$^{-1}$', fontsize = 11)
 
 ax2 = fig1.add_axes([0.15, 0.5, 0.75, 0.15])
 ax2.plot(global_heat_detrend['aredi_800']/1e22, color = 'k', label = 'aredi = 800 m$^2$s$^{-1}$')
-ax2.plot(heat_SH_anomaly['aredi_800'].data/1e22, color = 'k', ls = '--')
+ax2.plot(heat_SH_detrend['aredi_800']/1e22, color = 'k', ls = '--')
 plt.title('(b) A$_{redi}$ = 800 m$^2$s$^{-1}$', fontsize = 11)
 plt.ylabel('10$^{22}$ Joules')
 
 ax3 = fig1.add_axes([0.15, 0.25, 0.75, 0.15])
 ax3.plot(global_heat_detrend['aredi_2400']/1e22, color = 'k', label = 'aredi = 2400 m$^2$s$^{-1}$')
-ax3.plot(heat_SH_anomaly['aredi_2400'].data/1e22, color = 'k', ls = '--')
+ax3.plot(heat_SH_detrend['aredi_2400']/1e22, color = 'k', ls = '--')
 plt.title('(c) A$_{redi}$ = 2400 m$^2$s$^{-1}$', fontsize = 11)
 ax3.set_xlim([0,500])
 plt.savefig('notes/aredi_ohc_timeseries.png', bbox_inches='tight')
