56edf56d640133f26d2468be4373fbe8dc0e1d57
Jordan Thomas <jthom143@jhu.edu>
2016-05-02 09:23:15 -0400

    smooth and region functions

--- python/calc_region.py
+++ python/calc_region.py
@@ -17,6 +17,16 @@ def calc_six_regions(var):
 
     import iris
 
+    # Create a more useful longitude coordinate
+
+    lons = var.coord('longitude').points
+
+    for i in range(0, len(lons)):
+        if lons[i] < -180:
+            lons[i] = lons[i]+360
+
+    var.remove_coord('longitude')
+
     # Define constraints for each region:
     constraint_so = iris.Constraint(latitude=lambda y: -90 < y <= -50)
     constraint_spac = iris.Constraint(latitude=lambda y: -50 < y <= 0, longitude=lambda x: -283.5 <= x <= -60)
@@ -33,3 +43,26 @@ def calc_six_regions(var):
     var_arctic  = var.extract(constraint_arctic)
 
     return var_so, var_spac, var_satl, var_npac, var_natl, var_arctic 
+
+
+
+def calc_zonal_regions(var):
+    # Function to divide global oceans up into seven regions:
+    # Southern Ocean, Indian Ocean, S. Pacific, S. Atlantic, N. Pacific, N. Atlantic, and Arctic
+
+    import iris
+
+    # Define constraints for each region:
+    constraint_so = iris.Constraint(latitude=lambda y: -90 < y <= -50)
+    constraint_smidlats = iris.Constraint(latitude=lambda y: -50 < y <= -10)
+    constraint_nmidlats = iris.Constraint(latitude=lambda y: 10 < y <= 50)
+    constraint_arctic = iris.Constraint(latitude=lambda y: 50 < y <= 90)
+    constraint_tropics = iris.Constraint(latitude=lambda y: -10 < y <= 10)
+
+    var_so = var.extract(constraint_so)
+    var_smidlats = var.extract(constraint_smidlats)
+    var_nmidlats = var.extract(constraint_nmidlats)
+    var_arctic  = var.extract(constraint_arctic)
+    var_tropics = var.extract(constraint_tropics)
+
+    return var_so, var_smidlats, var_nmidlats, var_arctic, var_tropics
